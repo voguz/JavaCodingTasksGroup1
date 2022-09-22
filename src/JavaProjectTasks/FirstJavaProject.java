90a4aba34b068dc4b95d60fd046393b998ca8758
Volkan Oguz <oquzvolkan@gmail.com>
2022-09-22 11:19:56 -0500

    FirstJavaProject created.

---
+++ src/JavaProjectTasks/FirstJavaProject.java
@@ -0,0 +1,25 @@
+package JavaProjectTasks;
+
+public class FirstJavaProject {
+    public static void main(String[] args) {
+        /* Write a method which can identifies given number is even or odd
+        identify(5) --> "Odd"
+        identify(2) --> "Even"
+         */
+
+        /*
+        Write a method that can divide two numbers without using division operator
+         */
+
+        /*
+        Write a method which prints out the numbers from 1 to 30 but for numbers which are a multiple of 3, print "FIN" instead of the number and for numbers which are a multiple of 5, print "RA" instead of the number. For numbers which are a multiple of both 3 and 5, print "FINRA" instead of the numbers.
+         */
+
+
+
+
+
+
+
+    }
+}
