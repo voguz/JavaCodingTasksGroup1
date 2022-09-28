17e5ea33e807310b0bd9c37c41ea994da0f6c9b8
enas <enaselsaffar81@gmail.com>
2022-09-28 08:56:05 -0500

    update task

--- src/Group19/Task1.java
+++ src/Group19/Task1.java
@@ -34,20 +34,7 @@ public class Task1 {
 
             return count;
         }
-    }
 
-    public static void finra() {
-        for (int i = 1; i <= 30; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FINRA");
-            } else if (i % 3 == 0) {
-                System.out.println("FIN");
-
-            } else if (i % 5 == 0) {
-                System.out.println("RA");
-            }
-
-        }
     }
 }
 
