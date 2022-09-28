5e09aa78a5f67054afb2ee36baedd7a2cff7e54f
asikaer nadire <nadi9594@gmail.com>
2022-09-28 08:51:39 -0500

    Created Finra_ReturnMethod CLass

---
+++ src/JavaProjectTasks/Finra_ReturnMethod.java
@@ -0,0 +1,26 @@
+package JavaProjectTasks;
+
+public class Finra_ReturnMethod {
+    public static void main(String[] args) {
+
+
+        Finra();
+    }
+
+    public static void Finra() {
+
+        for (int i = 1; i <= 30; i++) {
+
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
