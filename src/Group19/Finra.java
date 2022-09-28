b1f9c637cde69f10678f6a21b037dcf014421df3
enas <enaselsaffar81@gmail.com>
2022-09-28 09:00:31 -0500

    update2 task

--- src/Group19/Finra.java
+++ src/Group19/Finra.java
@@ -1,21 +1,21 @@
 package Group19;
 
 public class Finra {
-    public static void main(String[] args) {
 
+   public static void finra(int num) {
 
-        for (int i = 1; i <= 30; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FINRA");
-            } else if (i % 3 == 0) {
-                System.out.println("FIN");
+       for (int i = 1; i <= 30; i++) {
+           if (i % 3 == 0 && i % 5 == 0) {
+               System.out.println("FINRA");
+           } else if (i % 3 == 0) {
+               System.out.println("FIN");
 
-            } else if (i % 5 == 0) {
+           } else if (i % 5 == 0) {
 
-                System.out.println("RA");
-            }
+               System.out.println("RA");
+           }
 
-        }
+       }
 
-    }
+   }
 }
