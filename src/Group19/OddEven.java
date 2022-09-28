53f998b674a0d58cfc4e7b3e377fd5fbe833652d
enas <enaselsaffar81@gmail.com>
2022-09-28 09:07:08 -0500

    create oddEven class

---
+++ src/Group19/OddEven.java
@@ -0,0 +1,13 @@
+package Group19;
+
+public class OddEven {
+
+    public static String oddEven(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else
+            System.out.println("this num is odd:" + num);
+        return "Odd";
+    }
+
+}
