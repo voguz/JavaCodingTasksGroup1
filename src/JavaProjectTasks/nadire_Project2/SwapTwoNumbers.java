35ff0a295cee591708cf219937b1906796b3a650
asikaer nadire <nadi9594@gmail.com>
2022-09-29 22:56:36 -0500

    swap numbers class created

---
+++ src/JavaProjectTasks/nadire_Project2/SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+package JavaProjectTasks.nadire_Project2;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        System.out.println("Originial value of a and b");
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+        System.out.println("After swap two values:");
+        a=a^b^(b=a);
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+
+
+
+
+
+    }
+}
