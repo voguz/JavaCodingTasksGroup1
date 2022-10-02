4414e3197d296aeac6245d4c8ad6f8b72f6ac46c
Volkan Oguz <oquzvolkan@gmail.com>
2022-10-02 18:17:02 -0500

    SecondJavaProject created.

---
+++ src/JavaProjectTasks/SecondJavaProject.java
@@ -0,0 +1,63 @@
+package JavaProjectTasks;
+
+public class SecondJavaProject {
+    public static void main(String[] args) {
+        /*
+         Swap Numbers
+         Swap two variable values without using a third variable
+        */
+
+        /*
+        Number - Divisible by 3,5,15
+        Write a program that can print the numbers between 1 ~ 100 that can be
+        divisible by 3, 5, and 15.
+        if the number can be divisible by 3, 5 and 15, then it should only be
+        displayed in Divisible By 15 section
+        if the number can be divisible by 3 but cannot be divisible by 15, then it
+        should only be displayed in Divisible By 3 section
+        if the number can be divisible by 5 but cannot be divisible by 15, then it
+        should only be displayed in Divisible By 5 section
+        ex:
+        OutPut:
+        Divisible By 15 15 30 45 60 75 90
+        Divisible By 5 5 10 20 25 35 40 50 55 65 70 80 85 95 100
+        Divisible By 3 3 6 9 12 18 21 24 27 33 36 39 42 48 51 54 57 63 66 69 72
+                     78 81 84 87 93 96 99
+
+         */
+
+        /*
+        Numbers - print consecutive numbers
+        Write a function:
+        that, given a positive integer N, prints the consecutive numbers from 1 to N,
+        each on a separate line. However, any number divisible by 2, 3 or 5 should
+        be replaced by the word Codility, Test or Coders respectively. If a number
+        is divisible by more than one of the numbers: 2,3 or 5, it should be replaced
+        by a concatenation of the respective words Codility, Test and Coders in this
+        given order. For example, numbers divisible by both 2 and 3 should be
+        replaced by Codility Test and numbers divisible by all three numbers: 2,3
+        and 5, should be replaced by Codility TestCoders.
+        For example, here is the output for N = 24:
+         1
+         Codility
+         Test
+         Codility
+         Coders
+         CodilityTest
+         7
+         Codility
+         Test
+         Codi1ityCoders
+         11
+         ...
+
+         */
+
+
+
+
+
+
+
+    }
+}
