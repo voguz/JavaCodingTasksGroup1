faa179793b44f8c925fa6120f88eacac4bcb00d9
Volkan Oguz <oquzvolkan@gmail.com>
2022-10-04 22:24:25 -0500

    SecondJavaProject created.

---
+++ src/Tasks/Task4Swap.java
@@ -0,0 +1,18 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task4Swap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of x and y");
+        int x = input.nextInt();
+        int y = input.nextInt();
+        System.out.println("Before swapping number " + "x: " + x + " y: " + y);
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("After swapping number " + "x: " + x + " y: " + y);
+
+    }
+}
