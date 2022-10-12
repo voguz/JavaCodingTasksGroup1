6feb49f863315f941ef590eb07bfe28618f6de68
Volkan Oguz <oquzvolkan@gmail.com>
2022-10-12 10:08:51 -0500

    Task 5 Number Divisible updated.

--- src/Tasks/Task5NumbersDivisible.java
+++ src/Tasks/Task5NumbersDivisible.java
@@ -11,12 +11,14 @@ public class Task5NumbersDivisible {
 
         System.out.println("\nDivided by 5: ");
         for (int i=1; i<100; i++) {
-            if (i % 5 == 0) System.out.print(i +", ");
+            if (i % 5 == 0)
+                System.out.print(i +", ");
         }
 
         System.out.println("\nDivided by 3 & 5: ");
         for (int i=1; i<100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) System.out.print(i +", ");
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.print(i +", ");
         }
     }
 }
