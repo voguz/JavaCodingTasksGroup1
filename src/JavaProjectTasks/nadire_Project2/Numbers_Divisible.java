68da025a1875016b0adb51adef10d70c2162faab
asikaer nadire <nadi9594@gmail.com>
2022-10-13 21:33:29 -0500

    project 3 created

--- src/JavaProjectTasks/nadire_Project2/Numbers_Divisible.java
+++ src/JavaProjectTasks/nadire_Project2/Numbers_Divisible.java
@@ -4,37 +4,25 @@ public class Numbers_Divisible {
     public static void main(String[] args) {
 
         System.out.print("Divisible By 15 ");
-for(int i=1;i<=100;i++){
-    if(i%3==0 && i%5==0 && i%15==0){
-        System.out.print(i+" ");
-    }
-}
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+                System.out.print(i + " ");
+            }
+        }
         System.out.println();
         System.out.print("Divisible By 3 ");
-for(int i=1;i<=100;i++){
-    if(i%3==0 && i%15!=0){
-        System.out.print(i+" ");
-    }
-}
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 15 != 0) {
+                System.out.print(i + " ");
+            }
+        }
         System.out.println();
         System.out.print("Divisible By 5 ");
-for(int i=1;i<=100;i++){
-    if(i%5==0 && i%15!=0){
-        System.out.print(i+" ");
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
+        for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0 && i % 15 != 0) {
+                System.out.print(i + " ");
+            }
+        }
 
 
     }
