649a8af6461b3040f01725a5e330a54df8c10fdc
Volkan Oguz <oquzvolkan@gmail.com>
2022-10-16 12:51:56 -0500

    Task 8 created.

---
+++ src/Tasks/Task8ArmStrongNumber.java
@@ -0,0 +1,31 @@
+package Tasks;
+
+public class Task8ArmStrongNumber {
+
+    //153= 1*1*1 + 5*5*5 + 3*3*3 = 153
+    public static void main(String[] args) {
+        int num = 153;
+        int number, temp, total = 0;
+
+        number = num;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == num)
+            System.out.println(num + " is an Armstrong number");
+        else
+            System.out.println(num + " is not an Armstrong number");
+
+
+
+
+
+
+
+
+    }
+    }
