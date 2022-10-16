273f549282f033f303ecebc8df96ede4244fdff8
Volkan Oguz <oquzvolkan@gmail.com>
2022-10-16 12:34:34 -0500

    Task 7  updated.

--- src/Tasks/Task7PrimeNumber.java
+++ src/Tasks/Task7PrimeNumber.java
@@ -1,35 +1,33 @@
 package Tasks;
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Task7PrimeNumber {
     public static void main(String[] args) {
 
 
-    Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
-    int number= input.nextInt();
-        if(isPrime(number)) {
-        System.out.println(number + " is prime number");
+        int number = input.nextInt();
+        if (Prime(number)) {
+            System.out.println(number + " is prime number");
+        } else {
+            System.out.println(number + " is NOT prime number");
+        }
     }
-        else{
-        System.out.println(number + " is a non-prime number");
-    }
-}
-    public static  boolean isPrime(int num)
-    {
-        if(num<=1)
-        {
+
+    public static boolean Prime(int num) {
+        if (num <= 1) {
             return false;
         }
-        for(int i = 2; i<= num/2; i++)
-        {
-            if(num%i==0)
-                return  false;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0)
+                return false;
+            }
+                return true;
+
         }
-        return true;
     }
 
-
-}
 
