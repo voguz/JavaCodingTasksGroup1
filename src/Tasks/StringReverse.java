3d9f0c18bbd40e60b2391567fb52faf89dbb42bc
Volkan Oguz <oquzvolkan@gmail.com>
2022-11-01 21:16:32 -0500

    String Reverse created.

---
+++ src/Tasks/StringReverse.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+public class StringReverse {
+    public static void main(String[] args) {
+
+        System.out.println(reverse("volkanoguz"));
+    }
+    public static String reverse(String rev) {
+
+        String reverseWord = "";
+
+        for (int i = rev.length() - 1; i >= 0; i--) {
+            reverseWord += rev.charAt(i);
+        }
+        return reverseWord;
+    }
+
+
+}
